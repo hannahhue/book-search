1d51020caeb822c81938e76cae2fa8cd030158cf
unknown <alicia9790972@gmail.com>
2022-11-21 23:45:22 -0500

    mutation front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $authors: [String]!) {
+    saveBook(userId: $userId, authors: $authors) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($userId: ID!, $bookId: String!) {
+    removeBook(userId: $userId, bookId: $bookId) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
